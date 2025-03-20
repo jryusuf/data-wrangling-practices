530f9799008ef16328d3ab4eb7ab9e98849f976b
jryusuf <yusufyuksel@aol.com>
2025-03-20 16:19:41 +0000

    initial draft

--- main.py
+++ main.py
@@ -6,7 +6,13 @@ def main():
 
     product_price = pd.read_csv("product_price.csv")
 
-    # your code here ...
+    #1 data cleaning and type conversion
+    #2 loyal customers logic
+    #3 standardized purchases logic
+    #4 join and filter loyal customers
+    #5 apply data filtering
+    #6 calculate total revenue and group
+    #7 order results
 
 
 if __name__ == "__main__":
